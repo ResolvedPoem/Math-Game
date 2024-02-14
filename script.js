81fbdddde0412e83456d61a4c3b1346fb6072338
aiyyer <gadavid@calpoly.edu>
2024-02-14 10:33:55 -0800

    Updates

--- script.js
+++ script.js
@@ -8,16 +8,66 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
 }
 
-//defining essential variables
+function chooseOp() {
+  let operator = ["+", "-", "*", "/"];
+  let rand = operator[Math.floor(Math.random()*operator.length)];
+  return rand;
+}
 
+function generateProblem() {
+  let num1 = getRandomInt(1,10);
+  let num2 = getRandomInt(1,10);
+  let operator = chooseOp();
+  let correct;
+    if (operator == "+"){
+      correct = num1 + num2;
+    }
+
+    else if (operator == "-"){
+      correct = num1 - num2;
+    }
+
+    else if (operator == "*"){
+      correct = num1 * num2;
+    }
+
+    else if (operator == "/"){
+      if (newQuestion = true) {
+        num1 = num1 * getRandomInt(1,4);
+        num2 = num2 * getRandomInt(1,4);
+      }
+
+      if (num1 < num2){
+        let numSwap = num1;
+        num1 = num2;
+        num2 = numSwap;
+      }
+      var remainder = num1%num2;
+      if (remainder != 0) {
+        num1 -= remainder;
+      }
+
+      correct = num1 / num2;
+    }
+  return [`${num1} ${operator} ${num2}`, correct];
+}
+
+//defining essential variables, arrays, and objects
+
+var initTime = Date.now();
+var timePassed = Date.now();
+var newQuestion = true;
+var answer = "";
+console.log(timePassed,initTime)
 
 //program begins
 
 function onStart() {
-  var num1 = getRandomInt(1,10);
-  var num2 = getRandomInt(1,10)
-  questions.innerHTML = `${num1} + ${num2}`;
+
+  while (timePassed <= initTime + 61000) {
+
+    var [problem, correct] = generateProblem();
+    document.getElementById("output").innerText = `What is: ${problem}`
+    timePassed = Date.now()
+  }
 }
-
-
-console.log("string")
